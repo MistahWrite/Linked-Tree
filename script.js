135d50d858b293ea2ef1df6296a5a19ce4f82590
Chad Jay Harrington <MistahWrite@gmail.com>
2021-03-20 15:02:50 -0600

    Worked on Justice Reskill Site

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function processForm() {
     let secretAnswer = document.getElementById("inputSecretAnswer").value;
     let message = document.getElementById("inputMessage").value;
     var preferredContactMethod = document.querySelector('input[name="input_radio_group_preferred"]:checked');
+//    let preferredContactMethodEmail = document.getElementById("input_radio_email").checked;
+//    let preferredContactMethodPhone = document.getElementById("input_radio_phone").checked;
+//    let preferredContactMethodNoContact = document.getElementById("input_radio_no_contact").checked;
     let income = document.getElementById("inputIncome").value;
     let correct = document.querySelector('input[name="inputCorrect"]:checked');
 
@@ -21,7 +24,9 @@ function processForm() {
     console.log(age);
     console.log(secretAnswer);
     console.log(message);
-    console.log(preferredContactMethod);
+//    console.log(preferredContactMethodEmail);
+//    console.log(preferredContactMethodPhone);
+//    console.log(preferredContactMethodNoContact);
     console.log(income);
     console.log(correct);
 
@@ -33,6 +38,9 @@ function processForm() {
     document.getElementById("outputSecretAnswer").value = secretAnswer;
     document.getElementById("outputMessage").value = message;
     document.getElementsByName("output_radio_email").value = preferredContactMethod;
+//    document.getElementById("output_radio_email").value = preferredContactMethodEmail;
+//    document.getElementById("output_radio_phone").value = preferredContactMethodPhone;
+//    document.getElementById("output_radio_no_contact").value = preferredContactMethodNoContact;
     document.getElementById("outputIncome").value = income;
     document.getElementById("outputCorrect").value = correct;
 
